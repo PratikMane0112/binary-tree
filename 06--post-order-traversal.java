001ae1419f388c4d4da0d961ad025f3d4a979d27
Pratik Mane <153143167+PratikMane0112@users.noreply.github.com>
2024-08-07 14:43:44 +0530

    Create 06--post-order-traversal.java

---
+++ 06--post-order-traversal.java
@@ -0,0 +1,53 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+// ------------------- Recursive approach------------------
+class Solution {
+    public List<Integer> postorderTraversal(TreeNode root) {
+        List<Integer> ans = new ArrayList<>();
+        TreeNode temp = root;
+        if(temp == null) return ans;
+        ans.addAll(postorderTraversal(temp.left));
+        ans.addAll(postorderTraversal(temp.right));
+        ans.add(temp.val);
+        return ans;
+      }
+}
+
+
+// Iterative approach -------------
+class Solution {
+    public List<Integer> postorderTraversal(TreeNode root) {
+        List<Integer> postorder = new ArrayList<>();
+        if (root == null) return postorder;
+        Stack<TreeNode> stack = new Stack<>();
+        stack.push(root);
+        TreeNode curr = root;
+        while (!stack.isEmpty()) {
+            curr = stack.pop();
+            // Ro, R, L -- reverse preorder traversal
+            while (curr != null) {
+                postorder.add(curr.val);
+                if (curr.left != null)
+                    stack.push(curr.left);
+                curr = curr.right;
+            }
+        }
+        // L, R, Ro
+        Collections.reverse(postorder);
+        return postorder;
+    }
+}
+// TC: O(n), SC: O(n)
